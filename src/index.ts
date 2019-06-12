b079cd8b679394237cc013f4454b9578b63cfbef
Emerson Almeida <emersonalmeidax@gmail.com>
2019-06-12 12:53:10 -0300

    add some coments

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,21 @@ export = (app: Application) => {
     app.on('issue_comment.created', async (context) => {
         if (context.isBot) { return }
 
+        // TODO: check the branch checks
+
         const { payload: { comment: { body } } } = context;
 
-        if (body.indexOf("@pr-pilot merge qa") > -1) {
+        if (body.indexOf("@pr-pilot merge qa") > -1) { // TODO: get the head branch from configuration
             const { data: { head: { ref } } } =
                 await context.github.pullRequests.get(context.repo(context.issue()))
 
             const { data: { sha } } =
-                await context.github.repos.merge(context.repo({ base: 'qa', head: ref }))
+                await context.github.repos.merge(context.repo({ base: 'qa', head: ref })) // TODO: get the head branch from configuration
+
+            // TODO: check if merge was successfully
 
             await context.github.issues.createComment(context.issue({ body: `Current brach was merged in QA, ${sha}` }))
+
             await context.github.issues.addLabels(context.issue({ labels: ['IN QA'] }))
         }
 
